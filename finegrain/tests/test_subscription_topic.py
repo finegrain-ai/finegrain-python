5fdf41010295b716f2ceab4df6ce48c36757e9ea
Pierre Chapuis <git@catwell.info>
2025-04-30 17:19:49 +0200

    support subscription topics

---
+++ finegrain/tests/test_subscription_topic.py
@@ -0,0 +1,43 @@
+import pytest
+
+from finegrain import EditorAPIContext, OKResult
+
+
+@pytest.mark.parametrize("subscription_topic", ["fg-test-topic", None])
+async def test_subscription_topic(
+    fx_base_url: str,
+    fx_credentials: str,
+    sofa_cushion_bytes: bytes,
+    subscription_topic: str | None,
+) -> None:
+    ctx = EditorAPIContext(
+        base_url=fx_base_url,
+        credentials=fx_credentials,
+        user_agent="finegrain-python-tests",
+        subscription_topic=subscription_topic,
+    )
+
+    await ctx.login()
+    await ctx.sse_start()
+
+    create_r = await ctx.call_async.create_state(
+        file=sofa_cushion_bytes,
+        meta={"test-key": "test-value"},
+    )
+    assert isinstance(create_r, OKResult)
+    assert create_r.meta["test-key"] == "test-value"
+
+    # to test credits update mechanism
+    assert isinstance(ctx.credits, int)
+    ctx.credits = None
+
+    infer_ms_r = await ctx.call_async.infer_product_name(create_r.state_id)
+    assert isinstance(infer_ms_r, OKResult)
+    assert infer_ms_r.meta["product_name"] == "sofa"
+
+    if subscription_topic is not None:
+        assert ctx.credits is None
+        await ctx.me()
+    assert isinstance(ctx.credits, int)
+
+    await ctx.sse_stop()
